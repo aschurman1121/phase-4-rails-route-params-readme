be8115d62c144902d0becdd5b319471a9d8b83d4
alexandra <alexandraschurman@gmail.com>
2022-03-15 09:36:30 -0700

    commit tree 707db2764b1f9d3f9f7a4b486028c346bf9175db

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -4,5 +4,11 @@ class CheesesController < ApplicationController
     cheeses = Cheese.all
     render json: cheeses
   end
-
+  
+  def show
+    # byebug  
+    cheese = Cheese.find(params[:id])
+    render json: cheese
+    
+  end
 end
